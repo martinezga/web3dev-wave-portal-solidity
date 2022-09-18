95f0fffcf2fd14c07254bbed9f422283c662f208
Gabriella Martinez <gmartinezviloria@buenosaires.gob.ar>
2022-09-18 19:16:30 -0300

    Added get all waves and hability to send message

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,32 +5,31 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
-    struct user{
-        address _address;
+    struct Wave{
+        address waver;
         uint256 id;
+        string message;
+        uint256 timestamp;
     }
     uint256 totalWaves;
-    mapping(address => user) userInfo;
+    event NewWave(address indexed from, uint256 id, uint256 timestamp, string message);
+    Wave[] waves;
 
     constructor() {
         console.log("Hello world Smart Contract!");
     }
 
-    function addUser(address waveSender, uint256 idSender) public {
-        userInfo[waveSender]._address = waveSender;
-        userInfo[waveSender].id = idSender;
-    }
-
-    function getUser() public view returns(address) {
-        // FIX: require(userInfo[msg.sender]._address == msg.sender, "You are not allowed.");
-
-        return userInfo[msg.sender]._address;
-    }
-
-    function wave() public {
+    function wave(string memory _message) public {
         totalWaves += 1;
-        addUser(msg.sender, totalWaves);
-        console.log("%s has waved!", msg.sender);
+        console.log("%s waved w/ message %s", msg.sender, _message);
+        // store the wave data in the array
+        waves.push(Wave(msg.sender, totalWaves, _message, block.timestamp));
+        // google for
+        emit NewWave(msg.sender, totalWaves, block.timestamp, _message);
+    }
+
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
     }
 
     function getTotalWaves() public view returns (uint256) {
